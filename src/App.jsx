6c3fbdaab596e7a2c0de6945a47ded587bd61148
EduardoRoMa19 <jor7_22@alu.uabcs.mx>
2024-09-17 11:09:38 -0600

    App.jsx
    Se cambio el nombre del pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="card">
         <div className="header">
-          <div className="pokemon-name">Oshawott</div>
+          <div className="pokemon-name">pikachu</div>
           <div className="pokemon-id">#501</div>
         </div>
         <div className="image-container">
